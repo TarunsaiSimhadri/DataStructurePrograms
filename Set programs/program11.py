30f8a73707c79e7bbf97f55d60787d2433b6c0e3
Tarunsai Simhadri <tarunsimhadri23@gmil.com>
2024-07-05 14:11:52 +0530

    [TarunSai] Add: Program to create an frozenset.

---
+++ Set programs/program11.py
@@ -0,0 +1,35 @@
+"""
+
+@Author: TarunSai
+@Date: 2024-07-04
+@Last Modified by: 
+@Last Modified time: 
+@Title : Program to create an frozenset.
+
+"""
+
+def frozen_set():
+
+    """
+
+    description:
+        This function is used create an frozenset.
+    
+    parameters:
+        None
+        
+    return:
+        none
+
+    """
+    frozen_set = frozenset([1, 2, 3, 4, 5])
+    print(frozen_set)
+          
+
+def main():
+
+    frozen_set()
+
+
+if __name__ == "__main__":
+    main()
